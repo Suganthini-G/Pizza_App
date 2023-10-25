78f23d95dc54f63b507ff0ea5f6a9db5dc18624e
Suganthini-G <suganthini29@gmail.com>
2023-10-25 14:40:08 +0530

    update image

--- Pizza_App/Pizza_App.Android/Resources/Resource.designer.cs
+++ Pizza_App/Pizza_App.Android/Resources/Resource.designer.cs
@@ -16670,25 +16670,31 @@ namespace Pizza_App.Droid
 			public const int pizza = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int pizza1 = 2131165330;
+			public const int Pizza01 = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int Pizza_sec = 2131165331;
+			public const int Pizza02 = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int preference_list_divider_material = 2131165332;
+			public const int pizza1 = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int Search_icon = 2131165333;
+			public const int Pizza_sec = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int test_custom_background = 2131165334;
+			public const int preference_list_divider_material = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int tooltip_frame_dark = 2131165335;
+			public const int Search_icon = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int tooltip_frame_light = 2131165336;
+			public const int test_custom_background = 2131165336;
+			
+			// aapt resource value: 0x7F070099
+			public const int tooltip_frame_dark = 2131165337;
+			
+			// aapt resource value: 0x7F07009A
+			public const int tooltip_frame_light = 2131165338;
 			
 			static Drawable()
 			{
